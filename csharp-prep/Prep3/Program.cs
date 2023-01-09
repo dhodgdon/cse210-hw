49201a36779e40892d2ccebd5ba96ec5343430c6
dhodgdon <hod20010@byui.edu>
2023-01-09 08:44:46 -0700

    Finished prep3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string again;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            string loop = "True";
+            int guessCount = 0;
+
+            while (loop == "True")
+            {
+                guessCount += 1;
+
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    loop = "False";
+                }
+            }
+
+            Console.WriteLine($"You made {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? ");
+            again = Console.ReadLine();
+        } while (again == "yes");
     }
 }
