b895a2d147f5211c68cd8f2587557f6e09341896
dhodgdon <hod20010@byui.edu>
2023-03-01 10:03:27 -0700

    Finished shape program.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("red", 8);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("blue", 3,5);
+        shapes.Add(r);
+
+        Circle c = new Circle("green", 1);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
